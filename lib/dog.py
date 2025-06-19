f2de3224621e52cb3ea7427de59394d2d777ce07
sumeyahussein17 <sumeyahussein21@gmail>
2025-06-19 04:47:17 -0700

    done

--- lib/dog.py
+++ lib/dog.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python3
 
 APPROVED_BREEDS = [
-    "Mastiff",
-    "Chihuahua",
-    "Corgi",
-    "Shar Pei",
-    "Beagle",
-    "French Bulldog",
-    "Pug",
-    "Pointer"
+    "Mastiff", "Chihuahua", "Corgi", "Shar Pei",
+    "Beagle", "French Bulldog", "Pug", "Pointer"
 ]
 
 class Dog:
-    pass
+    def __init__(self, name=None, breed=None):
+        self.name = None
+        self.breed = None
+
+        if name is not None:
+            if not isinstance(name, str) or not (1 <= len(name) <= 25):
+                print("Name must be string between 1 and 25 characters.")
+            else:
+                self.name = name
+
+        if breed is not None:
+            if breed not in APPROVED_BREEDS:
+                print("Breed must be in list of approved breeds.")
+            else:
+                self.breed = breed
--- lib/person.py
+++ lib/person.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python3
 
 APPROVED_JOBS = [
-    "Admin",
-    "Customer Service",
-    "Human Resources",
-    "ITC",
-    "Production",
-    "Legal",
-    "Finance",
-    "Sales",
-    "General Management",
-    "Research & Development",
-    "Marketing",
-    "Purchasing"
+    "Admin", "Customer Service", "Human Resources", "ITC", "Production",
+    "Legal", "Finance", "Sales", "General Management", "Research & Development",
+    "Marketing", "Purchasing"
 ]
 
 class Person:
-    pass
+    def __init__(self, name=None, job=None):
+        self.name = None
+        self.job = None
+
+        if name is not None:
+            if not isinstance(name, str) or not (1 <= len(name) <= 25):
+                print("Name must be string between 1 and 25 characters.")
+            else:
+                self.name = name.title()
+
+        if job is not None:
+            if job not in APPROVED_JOBS:
+                print("Job must be in list of approved jobs.")
+            else:
+                self.job = job
